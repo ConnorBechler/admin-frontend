434673a3ad4fbf3c69ede69845b1d7374870ee29
Russ <wernerru@msu.edu>
2024-07-09 00:10:53 -0400

    fix: enforce positive pay period lengths
    Signed-off-by: Russ <wernerru@msu.edu>

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import Vue from 'vue';
 import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
-import { required, email, min } from 'vee-validate/dist/rules';
+import { required, email, min, min_value } from 'vee-validate/dist/rules';
 import en from 'vee-validate/dist/locale/en';
 
 extend('password', {
@@ -23,6 +23,17 @@ extend('email', {
 extend('min', {
   ...min,
   message: en.messages['min']
+});
+
+extend('min_value', {
+  ...min_value,
+  message: en.messages['min_value']
+});
+
+extend('positive_value', {
+  ...min_value,
+  validate: (value ) => value >= 0,
+  message: '{_field_} must be positive'
 });
 
 extend('emailConfirm', {
